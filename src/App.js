9af4a189dd0ca24ab47f83a67d20b5dd7f4f6783
arjunuvladimer <arjunuvladimer@gmail.com>
2020-06-23 12:09:30 +0530

    Initial Design

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ class App extends React.Component{
     return (
     <div>
       <div className='focus-page'>
-        <h1>What are your main areas of focus?</h1>
-        <h2>This will help us build a personalized experience for you</h2>
+        <h1 style={{fontWeight:'500',fontSize:'28px'}}>What are your main areas of focus?</h1>
+        <h2 style={{fontWeight:'normal',fontSize:'19px'}}>This will help us build a personalized experience for you</h2>
           <div className="row">
   
             <div className="column">
@@ -81,6 +81,10 @@ class App extends React.Component{
           </div>
 
           
+      
+
+      </div>
+
       <div className='row-dot'>
           <div className='column-dot'>
             BACK
@@ -91,16 +95,13 @@ class App extends React.Component{
               <span className="dot" onclick="currentSlide(2)"></span>
               <span className="dot" onclick="currentSlide(3)"></span>
               <span className="dot" onclick="currentSlide(3)"></span>
-              <span className="dot" onclick="currentSlide(3)"></span>
+              <span className="dot active" onclick="currentSlide(3)"></span>
             </div>
           </div>
           <div className='column-dot'>
           DONE
           </div>
         </div>
-      </div>
-
-      
       
 
     </div>
