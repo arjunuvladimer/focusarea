91efd8083be28d143395b3a60814411ce3b8441a
arjunuvladimer <arjunuvladimer@gmail.com>
2020-06-23 15:33:24 +0530

    Small Style Change on Back Color and Padding'

--- src/pages/focus-page/focus-page.component.jsx
+++ src/pages/focus-page/focus-page.component.jsx
@@ -8,25 +8,28 @@ const FocusPage = () => (
         <h1 style={{fontWeight:'500',fontSize:'28px'}}>What are your main areas of focus?</h1>
         <h2 style={{fontWeight:'normal',fontSize:'19px'}}>This will help us build a personalized experience for you</h2>
         <Card />
+        <div className='row-dot'>
+        <div className='column-dot'>
+        BACK
+      </div>
+      <div className='column-dot'>
+        <div style={{textAlign:'center'}}>
+          <span className="dot"></span>
+          <span className="dot"></span>
+          <span className="dot"></span>
+          <span className="dot"></span>
+          <span className="dot active"></span>
+        </div>
+      </div>
+      <div className='column-dot'>
+        DONE
+      </div>
+        </div>
       </div>
 
-      <div className='row-dot'>
-          <div className='column-dot'>
-            BACK
-          </div>
-          <div className='column-dot'>
-            <div style={{textAlign:'center'}}>
-              <span className="dot"></span>
-              <span className="dot"></span>
-              <span className="dot"></span>
-              <span className="dot"></span>
-              <span className="dot active"></span>
-            </div>
-          </div>
-          <div className='column-dot'>
-            DONE
-          </div>
-      </div>
+      
+         
+      
     </>
 )
 
