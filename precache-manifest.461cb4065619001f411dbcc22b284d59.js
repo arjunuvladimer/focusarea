7101b23cadf49a29ff3b0cc63e50ed1a65e80a7d
arjunuvladimer <arjunuvladimer@gmail.com>
2020-06-23 12:20:31 +0530

    Updates

---
+++ precache-manifest.461cb4065619001f411dbcc22b284d59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61b880d2462b90b2929f462084061b8f",
+    "url": "/focusarea/index.html"
+  },
+  {
+    "revision": "1da0755f10231be27701",
+    "url": "/focusarea/static/css/main.b76d0afd.chunk.css"
+  },
+  {
+    "revision": "b547536e27cfd3681cb3",
+    "url": "/focusarea/static/js/2.fb289c74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/focusarea/static/js/2.fb289c74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1da0755f10231be27701",
+    "url": "/focusarea/static/js/main.c95292a8.chunk.js"
+  },
+  {
+    "revision": "fe59c77fcb16922ac144",
+    "url": "/focusarea/static/js/runtime-main.203d678b.js"
+  }
+]);
