1da73a58b699af5239a734f476c36539b8063472
arjunuvladimer <arjunuvladimer@gmail.com>
2020-06-23 11:02:11 +0530

    Basic Layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,111 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      backgroundColor:false
+    }
+  }
+
+  handleClick =  () => {
+    this.setState({
+      backgroundColor: !this.state.backgroundColor
+    })
+  } 
+  render(){
+    return (
+    <div>
+      <div className='focus-page'>
+        <h1>What are your main areas of focus?</h1>
+        <h2>This will help us build a personalized experience for you</h2>
+          <div className="row">
+  
+            <div className="column">
+               <div className={`${this.state.backgroundColor ? 'bgColor':''} card`} onClick={this.handleClick}>
+                  <div className="container">
+                    <h3><b>Blood Glucose</b></h3>
+                  </div>
+                </div>
+              </div>
+  
+            <div className="column">
+              <div className="card">
+                <div className="container">
+                  <h3><b>Medication</b></h3>
+                </div>
+              </div>
+            </div>
+
+            <div className="column">
+              <div className="card">
+                <div className="container">
+                  <h3><b>Food Tracking</b></h3>
+                </div>
+              </div>
+            </div>
+
+            <div className="column">
+              <div className="card">
+                <div className="container">
+                  <h3><b>Exercise</b></h3>
+                </div>
+              </div>
+            </div>
+
+            <div className="column">
+              <div className="card">
+                <div className="container">
+                  <h3><b>Weight Management</b></h3>
+                </div>
+              </div>
+            </div>
+
+            <div className="column">
+              <div className="card">
+                <div className="container">
+                  <h3><b>Blood Pressure</b></h3>
+                </div>
+              </div>
+            </div>
+
+            <div className="column">
+              <div className="card">
+                <div className="container">
+                  <h3><b>A1C</b></h3>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          
+      <div className='row-dot'>
+          <div className='column-dot'>
+            BACK
+          </div>
+          <div className='column-dot'>
+            <div style={{textAlign:'center'}}>
+              <span className="dot" onclick="currentSlide(1)"></span>
+              <span className="dot" onclick="currentSlide(2)"></span>
+              <span className="dot" onclick="currentSlide(3)"></span>
+              <span className="dot" onclick="currentSlide(3)"></span>
+              <span className="dot" onclick="currentSlide(3)"></span>
+            </div>
+          </div>
+          <div className='column-dot'>
+          DONE
+          </div>
+        </div>
+      </div>
+
+      
+      
+
     </div>
-  );
+    )
+  }
 }
 
 export default App;
